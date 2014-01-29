9dabc222988b529eb0ce9d295051dba83ee40ad5
Jip van Reijsen <jipvanreijsen@gmail.com>
2014-01-29 14:21:01 +0100

    Introduce IRC::Prefix

---
+++ lib/irc/prefix.rb
@@ -0,0 +1,102 @@
+module IRC
+  # Public: Represents a Message. This class is intended to be subclassed. Its
+  # `#parse` and `#valid?` methods are not implemented and should be redefined.
+	class Prefix
+    # Public: Gets the raw message String.
+    attr_reader :raw
+
+    # Public: Gets the nick String.
+    attr_reader :nick
+
+    # Public: Gets the user String.
+    attr_reader :user
+
+    # Public: Gets the host String.
+    attr_reader :host
+
+    # Public: Initializes the prefix.
+    def initialize(raw_prefix)
+      @raw = raw_prefix
+
+      @nick, @user, @host = parse raw_prefix
+    end
+
+    # Internal: Parses a raw prefix. It should return an Array of three elements
+    # (nick, user, host).
+    #
+    # raw_prefix - A raw prefix String.
+    #
+    # Returns an Array of three elements.
+    # Raises NotImplementedError when not implemented (default).
+    def parse(raw_prefix)
+      raise NotImplementedError, "#{self}#parse is not implemented."
+    end
+
+    # Public: Checks the prefix for validity. It should return `true` when the 
+    # prefix is valid, `false` otherwise.
+    #
+    # Returns true or false.
+    # Raises NotImplementedError when not implemented (default).
+    def valid?
+      raise NotImplementedError, "#{self}#valid? is not implemented."
+    end
+
+    # Public: Checks the equality of the prefix and other. The comparison is
+    # case-insensitive (including conversion of []\~ to {}|^, see: 
+    # https://tools.ietf.org/html/rfc2812#section-2.2). Wildcards are 
+    # ignored.
+    def ==(other)
+      unless other.respond_to? :to_str
+        raise TypeError, "no implicit conversion of #{other.class} into String"
+      end
+
+      downcased_self  = self.to_s.tr("A-Z[]\\\\~", "a-z{}|^")
+      downcased_other = other.to_str.tr("A-Z[]\\\\~", "a-z{}|^")
+
+      downcased_self == downcased_other
+    end
+
+    # Public: Checks the equality of the prefix and other. The comparison is
+    # case-insensite (including conversion of []\~ to {}|^, see:
+    # https://tools.ietf.org/html/rfc2812#section-2.2). Wildcards are 
+    # honored. Uses #to_regexp under the hood.
+    def =~(other)
+      self.to_regexp =~ other
+    end
+
+    def to_a
+      [self.nick, self.user, self.host]
+    end
+
+    def to_h
+      { :nick => self.nick, :user => self.user, :host => self.host }
+    end
+
+    def to_regexp
+      # This is an ugly hack and should be replaced with something beautiful.
+      escaped  = Regexp.escape to_s
+      prepared = escaped.gsub(/(\\.|~)/) do |char|
+        CONVERSIONS[char[-1,1]] || char
+      end
+
+      Regexp.new "^#{prepared}$", Regexp::IGNORECASE
+    end
+
+    def to_s
+      "#{@nick || "*"}!#{@user || "*"}@#{@host || "*"}"
+    end
+
+    alias_method :to_str, :to_s
+
+  private
+
+    CONVERSIONS = {
+      '?'  => '\S',
+      '*'  => '\S*',
+      '['  => '(\[|\{)',   '{' => '(\[|\{)',
+      ']'  => '(\]|\})',   '}' => '(\]|\})',
+      '\\' => '(\\\\|\|)', '|' => '(\\\\|\|)', # one of the backslashes will be 
+      '~'  => '(~|\^)',    '^' => '(~|\^)'     # replaced by the gsub. so gross.
+    }
+	end
+end
---
+++ test/irc/prefix_test.rb
@@ -0,0 +1,124 @@
+require_relative "../test_helper"
+require "irc/prefix"
+
+class TestPrefix < IRC::Prefix
+  def parse(_)
+    ["nick", "user", "host.com"]
+  end
+end
+
+class TestMask < IRC::Prefix
+  def parse(_)
+    ["n?ck", "*", "h*st.com"]
+  end
+end
+
+# Because of IRC's Scandinavian origin, the characters {}|^ are considered to be
+# the lower case equivalents of the characters []\~, respectively. This is a 
+# critical issue when determining the equivalence of two nicknames or channel 
+# names.
+
+class ScandinavianPrefix < IRC::Prefix
+  def parse(_)
+    ["{}|^abc[]\\~", "user", "host.com"]
+  end
+end
+
+setup { TestPrefix.new "" }
+
+# Prefix parts
+
+test "the raw prefix" do |prefix|
+  assert_equal prefix.raw, ""
+end
+
+test "the nick" do |prefix|
+  assert_equal prefix.nick, "nick"
+end
+
+test "the user" do |prefix|
+  assert_equal prefix.user, "user"
+end
+
+test "the host" do |prefix|
+  assert_equal prefix.host, "host.com"
+end
+
+# Equality operators
+
+test "#== without wildcards" do |prefix|
+  assert prefix == "nick!user@host.com"
+  assert prefix == "NICK!user@host.com"
+  assert prefix == prefix
+end
+
+test "#== with wildcards" do
+  mask = TestMask.new "n?ck!*@h*st.com"
+
+  assert (not mask == "nick!user@host.com")
+  assert (not mask == "NICK!user@host.com")
+end
+
+test "#== for scandinavians" do
+  scandinavian = ScandinavianPrefix.new "{}|^abc[]\\~!user@host.com"
+  
+  assert scandinavian == "{}|^abc{}|^!user@host.com"
+  assert scandinavian == "{}|^abc[]\\~!user@host.com"
+  assert scandinavian == "[]\\~abc{}|^!user@host.com"
+  assert scandinavian == "[]\\~abc[]\\~!user@host.com"
+end
+
+test "#=~ without wildcards" do |prefix|
+  assert prefix =~ "nick!user@host.com"
+  assert prefix =~ "NICK!user@host.com"
+  assert prefix =~ prefix
+end
+
+test "#=~ with wildcards" do
+  mask = TestMask.new "n?ck!*@h*st.com"
+
+  assert      mask =~ "nick!user@host.com"
+  assert      mask =~ "nack!user@haast.com"
+  assert (not mask =~ "naack!user@haast.com") # ? matches a single character
+end
+
+test "#=~ for scandinavians" do
+  scandinavian = ScandinavianPrefix.new "{}|^abc[]\\~!user@host.com"
+
+  assert scandinavian =~ "{}|^abc{}|^!user@host.com"
+  assert scandinavian =~ "{}|^abc[]\\~!user@host.com"
+  assert scandinavian =~ "[]\\~abc{}|^!user@host.com"
+  assert scandinavian =~ "[]\\~abc[]\\~!user@host.com"
+end
+
+# Conversions
+
+test "to_a" do |prefix|
+  assert_equal prefix.to_a, ["nick", "user", "host.com"]
+end
+
+test "to_h" do |prefix|
+  assert_equal prefix.to_h, { 
+    :nick => "nick", 
+    :user => "user", 
+    :host => "host.com"
+  }
+end
+
+test "to_regexp" do |prefix|
+  mask         = TestMask.new "n?ck!*@h*st.com"
+  scandinavian = ScandinavianPrefix.new "{}|^abc[]\\~!user@host.com"
+
+  assert_equal prefix.to_regexp, /^nick!user@host\.com$/i
+  assert_equal mask.to_regexp, /^n\Sck!\S*@h\S*st\.com$/i
+  assert_equal scandinavian.to_regexp, # this is motherflippin ridiculous.
+  /^(\[|\{)(\]|\})(\\|\|)(~|\^)abc(\[|\{)(\]|\})(\\|\|)(~|\^)!user@host\.com$/i
+end
+
+test "to_s" do |prefix|
+  assert_equal prefix.to_s, "nick!user@host.com"
+end
+
+test "to_str" do |prefix|
+  assert_equal prefix.to_str, "nick!user@host.com"
+end
