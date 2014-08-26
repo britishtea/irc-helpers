fa6983ec92f55729af54397d7cec549f2cf3598b
Jip van Reijsen <jipvanreijsen@gmail.com>
2014-08-26 02:24:28 +0200

    Move IRC::Message#numeric? and IRC::Message#error? to IRC::RFC2812::Message

--- lib/irc/rfc2812/message.rb
+++ lib/irc/rfc2812/message.rb
@@ -11,6 +11,21 @@ module IRC
       def valid?
         Helpers.valid? self.raw
       end
+
+      # Public: Checks if the message is a numeric reply.
+      def numeric?
+        self.command =~ /^\d+$/
+      end
+
+      # Public: Checks if the message is an error reply according to RFC2812.
+      def error?
+        self.numeric? && ERROR_REPLY_RANGE.cover?(self.command)
+      end
+
+    private
+
+      # Internal: A range of error reply codes.
+      ERROR_REPLY_RANGE = :"400"...:"599"
     end
   end
 end
