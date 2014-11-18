dc9a3a34bb6bbf8852f362a039feb592bee961aa
Jip van Reijsen <jipvanreijsen@gmail.com>
2014-11-19 00:12:26 +0100

    Introduce IRC::RFC2812::Whois

---
+++ lib/irc/rfc2812/whois.rb
@@ -0,0 +1,97 @@
+module IRC
+  module RFC2812
+    # Public: Represents a WHOIS response.
+    class Whois
+      attr_reader :nick, :user, :host, :realname, :channels, :server, 
+        :server_info, :seconds_idle
+
+      # Public: Gets the statuses Hash (`{ "#channel" => :status }`). The status
+      # is `nil` if there is no status.
+      attr_reader :statuses
+
+      # Public: Initializes the ...
+      #
+      # messages - An Array of IRC::Messages.
+      def initialize(messages)
+        @channels = []
+        @statuses = {}
+
+        messages.each do |message|
+          method = "parse_#{message.command}"
+          
+          if self.respond_to? method, true
+            self.send method, message
+          end
+        end
+      end
+
+      def away?
+        @away || false
+      end
+
+      # Public: Operator means IRC operator, not channel operator.
+      def operator?
+        @operator || false
+      end
+
+      # Public: Gets the channels. If a `status` is given, it returns only the
+      # channels with that status.
+      #
+      # status - A status Symbol (default: false).
+      #
+      # Returns an Array.
+      def channels(status = false)
+        if status == false
+          @channels
+        elsif status.nil?
+          @statuses.select { |_, s| s.nil? }.map(&:first)
+        else
+          @statuses.select { |_, s| status.to_sym == s }.map(&:first)
+        end
+      end
+
+    private
+
+      def parse_311(message)
+        @nick     = message.params[1]
+        @user     = message.params[2]
+        @host     = message.params[3]
+        @realname = message.trail
+      end
+      
+      def parse_319(message)
+        # Channels with the "+" prefix don't support modes.
+        parsed = message.to_s.split.map do |channel|
+          if channel.start_with?("++")
+            [nil, channel]
+          else
+            channel.match(/([^#&!])? ([#&!+]\S+)/x).captures
+          end
+        end
+
+        @channels += parsed.map(&:last)
+
+        parsed.each do |status, channel|
+          @statuses[channel] = status.nil? ? nil : status.to_sym
+        end
+      end
+
+      def parse_312(message)
+        @server      = message.params[2]
+        @server_info = message.trail
+      end
+
+      def parse_301(message)
+        @away = true
+      end
+
+      def parse_313(message)
+        @operator = true
+      end
+
+      def parse_317(message)
+        @seconds_idle = message.params[2].to_i
+      end
+    end
+  end
+end
---
+++ test/irc/rfc2812_whois_test.rb
@@ -0,0 +1,94 @@
+require_relative "../test_helper"
+require "irc/rfc2812/whois"
+require "irc/rfc2812/message"
+
+def msg(*args)
+  IRC::RFC2812::Message.new *args
+end
+
+MESSAGES = [
+  msg(":prefix 311 x nick user host.com * :real name\r\n"), # RPL_WHOISUSER
+  msg(":prefix 319 x nick :@#one +#two ##three ++four\r\n"),# RPL_WHOISCHANNELS
+  msg(":prefix 312 x nick server.com :server info"),        # RPL_WHOISSERVER
+  msg(":prefix 301 x nick :away message\r\n"),              # RPL_AWAY
+  msg(":prefix 313 x nick :is an IRC operator\r\n"),        # RPL_WHOISOPERATOR
+  msg(":prefix 317 x nick 15 :seconds idle\r\n"),           # RPL_WHOISIDLE
+  msg(":prefix 318 x nick :End of WHOIS list\r\n")          # RPL_ENDOFWHOIS
+]
+
+setup { IRC::RFC2812::Whois.new MESSAGES }
+
+# RPL_WHOISUSER
+
+test "#nick" do |whois|
+  assert_equal whois.nick, "nick"
+end
+
+test "#user" do |whois|
+  assert_equal whois.user, "user"
+end
+
+test "#host" do |whois|
+  assert_equal whois.host, "host.com"
+end
+
+test "#realname" do |whois|
+  assert_equal whois.realname, "real name"
+end
+
+# RPL_WHOISCHANNELS
+
+test "#channels" do |whois|
+  assert_equal whois.channels, ["#one", "#two", "##three", "++four"]
+end
+
+test "#channels with a status symbol" do |whois|
+  assert_equal whois.channels("@"), ["#one"]
+  assert_equal whois.channels(:+),  ["#two"]
+  assert_equal whois.channels(nil), ["##three", "++four"]
+end
+
+test "#statuses" do |whois|
+  assert_equal whois.statuses, "#one" => :"@", "#two" => :+, "##three" => nil, 
+                               "++four" => nil
+end
+
+# RPL_WHOISSERVER
+
+test "the server" do |whois|
+  assert_equal whois.server, "server.com"
+end
+
+test "server info" do |whois|
+  assert_equal whois.server_info, "server info"
+end
+
+# RPL_AWAY
+
+test "#away? (while away)" do |whois|
+  assert_equal whois.away?, true
+end
+
+test "#away? (while not away)" do |whois|
+  whois = whois.class.new [MESSAGES.last]
+
+  assert_equal whois.away?, false
+end
+
+# RPL_WHOISOPERATOR
+
+test "#operator? (while operator)" do |whois|
+  assert_equal whois.operator?, true
+end
+
+test "#operator? (while not operator)" do |whois|
+  whois = whois.class.new [MESSAGES.last]
+
+  assert_equal whois.operator?, false
+end
+
+# RPL_WHOISIDLE
+
+test "idle time" do |whois|
+  assert_equal whois.seconds_idle, 15
+end
