4eac6b595edd3cf0b400063a071a3486775001cd
Jip van Reijsen <jipvanreijsen@gmail.com>
2015-02-18 17:27:21 +0100

    Add IRC::RFC2812::Message#target_channel

--- lib/irc/rfc2812/message.rb
+++ lib/irc/rfc2812/message.rb
@@ -1,9 +1,12 @@
 require "irc/message"
+require "irc/rfc2812/constants"
 require "irc/rfc2812/helpers"
 
 module IRC
   module RFC2812
     class Message < IRC::Message
+      include Constants
+
       def self.parse(raw_message)
         Helpers.parse raw_message
       end
@@ -20,6 +23,26 @@ module IRC
       # Public: Checks if the message is an error reply according to RFC2812.
       def error?
         self.numeric? && ERROR_REPLY_RANGE.cover?(self.command)
+      end
+
+      # Public: Extracts the target channel.
+      #
+      # Returns a String or nil.
+      def target_channel
+        name = case self.command
+          when :invite              then self.params[1]
+          when RPL_NAMREPLY         then self.params[2]
+          when ERR_USERNOTINCHANNEL then self.params[2]
+          when ERR_USERONCHANNEL    then self.params[2]
+          when ERR_UNKNOWNMODE      then self.trail[/\S+$/]
+          else                           self.params.fetch(numeric? ? 1 : 0, "")
+        end
+
+        if name.start_with?('&', '#', '+', '!')
+          return name
+        end
+
+        return nil
       end
 
     private
--- test/irc/rfc2812_message_test.rb
+++ test/irc/rfc2812_message_test.rb
@@ -1,6 +1,10 @@
 require_relative "../test_helper"
 require "irc/rfc2812/message"
 
+def msg(message)
+  IRC::RFC2812::Message.new(message)
+end
+
 setup { IRC::RFC2812::Message.new ":prefix COMMAND para meter :the trail\r\n" }
 
 test "#prefix returns an IRC::RFC2812::Prefix" do |message|
@@ -26,4 +30,37 @@ test "#error?" do |message|
   assert (not message.class.new("399").error?)
   assert (not message.class.new("600").error?)
   assert (not message.error?)
+end
+
+test "#target_channel" do
+  positives = [
+    msg(":prefix JOIN #channel\r\n"),
+    msg(":prefix PART #channel :Reason\r\n"),
+    msg(":prefix MODE #channel -l\r\n"),
+    msg(":prefix TOPIC #channel :Topic\r\n"),
+    msg(":prefix INVITE user #channel\r\n"),
+    msg(":prefix KICK #channel user :Reason\r\n"),
+    msg(":prefix PRIVMSG #channel :Message\r\n"),
+    msg(":prefix NOTICE #channel :Notice\r\n"),
+    msg(":prefix 353 us = #channel :@user +other_user\r\n"),
+    msg(":prefix 441 us user #channel :They aren't on that channel\r\n"),
+    msg(":prefix 443 us user #channel :is already on channel\r\n"),
+    msg(":prefix 472 us <char> :is unknown mode char to me for #channel\r\n"),
+  ]
+
+  positives.each do |message|
+    assert_equal message.target_channel, "#channel"
+  end
+
+  negatives = [
+    msg(":prefix NICK user\r\n"),
+    msg(":prefix QUIT :Reason\r\n"),
+    msg(":prefix SQUIT :Reason\r\n"),
+    msg(":prefix PRIVMSG user :Message\r\n"),
+    msg(":prefix NOTICE user :Notice\r\n"),
+  ]
+
+  negatives.each do |message|
+    assert_equal message.target_channel, nil
+  end 
 end
